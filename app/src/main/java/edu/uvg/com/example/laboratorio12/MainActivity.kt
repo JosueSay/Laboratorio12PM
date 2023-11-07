7c538d7f960964fc7c92db33fa04752fd6d1b7ea
JosueSay <106031855+JosueSay@users.noreply.github.com>
2023-11-07 10:03:18 -0600

    Finalizacion_densidad_telefono

--- app/src/main/java/edu/uvg/com/example/laboratorio12/MainActivity.kt
+++ app/src/main/java/edu/uvg/com/example/laboratorio12/MainActivity.kt
@@ -1,11 +1,34 @@
 package edu.uvg.com.example.laboratorio12
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
+
+
+    private var peliculas = emptyArray<String>()
+    lateinit var arrayAdapter: ArrayAdapter<*>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val peliculas = arrayOf(
+            "Terminator",
+            "Avengers",
+            "Antman",
+            "Soy leyenda",
+            "Avatar",
+            "It"
+        )
+
+        val itemsAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, peliculas)
+        val listaPeliculas: ListView = findViewById(R.id.listaPeliculas)
+        listaPeliculas.adapter = itemsAdapter
+
+
     }
 }
